0b6a3ded9b43b0bd151d85be30251c4b4a5a5514
UltimatePlayer97 <gogole.help134@gmail.com>
2026-09-14 12:24:25 +1000

    chore: update embeds

--- src/pages/Invite.tsx
+++ src/pages/Invite.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useMemo, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
-import DiscordMeta from "../components/DiscordMeta";
 
 const WORKER_BASE = "https://roblox-proxy.proxy-roblox.workers.dev";
 
@@ -106,9 +105,6 @@ const Invite: React.FC = () => {
 
   return (
     <HelmetProvider>
-      {/* Discord Meta Tags */}
-      {placeId && <DiscordMeta placeId={placeId} />}
-
       <main className="min-h-[60vh] flex items-center justify-center p-6">
         <div className="max-w-3xl w-full flex gap-6 items-start">
           {/* Thumbnail + Game Info */}
